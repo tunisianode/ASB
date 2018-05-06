b3d7116936f63a771e37a9961b98f6b8b35a19bb
Amor Aissaoui <tunisianode@gmail.com>
2018-05-06 13:45:26 +0200

    add Buffer to producerconsumer

---
+++ pp/producerconsumer/Buffer.java
@@ -0,0 +1,42 @@
+package pp.producerconsumer;
+
+public class Buffer
+{
+
+    private boolean available = false;
+    private int data;
+
+    public synchronized void put(int x)
+    {
+        while(available)
+        {
+            try
+            {
+                wait();
+            }
+            catch(InterruptedException e)
+            {
+            }
+        }
+        data = x;
+        available = true;
+        notify();
+    }
+
+    public synchronized int get()
+    {
+        while(!available)
+        {
+            try
+            {
+                wait();
+            }
+            catch(InterruptedException e)
+            {
+            }
+        }
+        available = false;
+        notify();
+        return data;
+    }
+}
